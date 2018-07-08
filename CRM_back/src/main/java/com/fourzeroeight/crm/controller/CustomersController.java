b3fee4aad317407a59000ff422e3f9666fe37a1c
AdsionLi <adsionli@foxmail.com>
2018-07-08 08:48:48 +0800

    完成重复性劳动

--- CRM_back/src/main/java/com/fourzeroeight/crm/controller/CustomersController.java
+++ CRM_back/src/main/java/com/fourzeroeight/crm/controller/CustomersController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/contacts")
+@RequestMapping("/api/Customers")
 public class CustomersController {
 
     @Autowired
