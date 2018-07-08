71926e70ef18b779ee4d4da05afa770b36083f80
readH <m18867836322@163.com>
2018-07-08 15:02:55 +0800

    2018-07-08 下午2

--- CRM_back/src/main/java/com/fourzeroeight/crm/bean/Files.java
+++ CRM_back/src/main/java/com/fourzeroeight/crm/bean/Files.java
@@ -1,5 +1,8 @@
 package com.fourzeroeight.crm.bean;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.util.Date;
 
 public class Files {
@@ -15,6 +18,8 @@ public class Files {
 
     private String type;
 
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private Date createtime;
 
     private Integer custid;
