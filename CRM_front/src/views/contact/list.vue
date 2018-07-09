7ab783779c428a0269d5857d6b844ec48bc7eaf8
Robinson28years <bb.chen@outlook.com>
2018-07-09 14:29:49 +0800

    数据添加

--- CRM_front/src/views/contact/list.vue
+++ CRM_front/src/views/contact/list.vue
@@ -317,7 +317,7 @@
           "page": 1,
           "offset": 0,
           "search": "",
-          "searchName": "username",
+          "searchName": "custname",
           "order": "ASC",
           "orderName": "id",
           "start": null,
